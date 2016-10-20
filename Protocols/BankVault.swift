4be4c81974ee1db8be1e9af0d8c069abc8e84a70
jicaey <michaelyoung.dev@gmail.com>
2016-10-20 18:43:51 -0400

    Lab Complete

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,6 +20,27 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
+
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        var isCorrectPassword = false
+        
+        if 1...10 ~= password.count {
+            for (index, value) in password.enumerated() {
+                if index % 2 == 0 && value % 2 == 0 {
+                    isCorrectPassword = true
+                } else {
+                    break
+                }
+            }
+        }
+        return isCorrectPassword
+    }
+}
+
 
 
 
